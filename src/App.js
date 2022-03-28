f9d928c777bf46496ce5ae5b641e3881f2bbb370
julienbrg <julien@strat.cc>
2022-03-28 19:43:18 +0200

    rm old css

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
     href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
     rel="stylesheet"
   />
-  <link rel="stylesheet" href="./css/master.css" />
   <header>
     <img
       src={logo}
